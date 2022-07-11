930c493c1e95ff54ab86a338f053d9d5766471c6
Pedro Zago <pedro.camargozago@hotmail.com.br>
2022-07-11 17:28:52 -0300

    feat(config): project settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,15 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['eslint-plugin-import-helpers'],
+  rules: {
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: ['module', ['parent', 'sibling', 'index']],
+        alphabetize: { order: 'asc', ignoreCase: true },
+      },
+    ],
+  },
 };
